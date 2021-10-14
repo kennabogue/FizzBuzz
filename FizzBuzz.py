3b931326b8decdd72510e1efe350fb139b78b8c1
Kenna Bogue <75717592+kennabogue@users.noreply.github.com>
2021-10-14 15:14:03 -0500

    Add files via upload

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -4,18 +4,7 @@
 
 print('FizzBuzz Challenge')
 print('----------------------------------')
-
-count = range(1, 101)
-
-for i in count:
-    if i % 15 == 0:
-        print('FizzBuzz')
-    elif i % 3 == 0:
-        print('Fizz')
-    elif i % 5 == 0:
-        print('Buzz')
-    elif i % 3 != 0 and i % 5 !=0 and i % 15 != 0:
-        print(i)
     
-    
-    
+FizzBuzz = ["FizzBuzz" if (i%3 == 0 and i %5 == 0) else "Fizz" if (i%3 == 0) else "Buzz" if (i%5 == 0) else i for i in range(1,101)]
+for i in FizzBuzz:
+    print(i)
